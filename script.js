2dede8ec0f785c52ab1b4a7eacfb2db3e4777e7e
Nick00123456789 <ari74s@hotmail.com>
2025-03-12 14:44:42 +0200

    u

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-        const SUPABASE_URL = "https://kigbtbacxkfeevmyvioa.supabase.co";
-        const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZ2J0YmFjeGtmZWV2bXl2aW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MTgyNzQsImV4cCI6MjA1NzI5NDI3NH0.OqMLrZ2NzZ6CMNaZcKKHBA7V1PTQfy7g5MKv8XT1-N4";
-        const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+       
+        const supabase = createClient('https://kigbtbacxkfeevmyvioa.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZ2J0YmFjeGtmZWV2bXl2aW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MTgyNzQsImV4cCI6MjA1NzI5NDI3NH0.OqMLrZ2NzZ6CMNaZcKKHBA7V1PTQfy7g5MKv8XT1-N4');
+
 
         const authScreen = document.getElementById("auth-screen");
         const chatScreen = document.getElementById("chat-screen");
